97bcb06e5ef44f8c6bc352b27d4209aa60442a4f
salasalvardoerickfernando@gmail.com <salasalvardoerickfernando@gmail.com>
2024-12-31 19:23:54 +0000

    Edited HomeForm

--- client_code/HomeForm/__init__.py
+++ client_code/HomeForm/__init__.py
@@ -1,7 +1,7 @@
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.http
+ import anvil.http
 import  as pd  # Correct import for pandas
 
 
